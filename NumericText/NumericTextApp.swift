daac1e2e902a5e2f1a08ca143fd206941d164813
Илья Аникин <i.anikin@wallpaperscraft.com>
2024-11-14 12:13:24 +0300

    project template

---
+++ NumericText/NumericTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumericTextApp.swift
+//  NumericText
+//
+//  Created by Илья Аникин on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NumericTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
